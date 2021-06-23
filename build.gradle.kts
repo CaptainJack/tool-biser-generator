0550b77b21c4761dd7d976a43f6a54184ec5f7ad
Alexey Chuprin <a.chuprin@capjack.ru>
2021-06-23 10:57:49 +0300

    Develop

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,8 @@ dependencies {
 	implementation("ru.capjack.tool:tool-logging:1.5.0")
 	implementation("ru.capjack.tool:tool-lang:1.11.1")
 	implementation("ru.capjack.tool:tool-utils:1.6.1")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.+")
-	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.+")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.3")
+	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.3")
 	
 	testImplementation(kotlin("test"))
 	testImplementation("ch.qos.logback:logback-classic:1.2.3")
