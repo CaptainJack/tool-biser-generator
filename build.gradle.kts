ee8ad03bd7238d6ed4e25ea450c6a1964bcd8e13
Alexey Chuprin <a.chuprin@capjack.ru>
2022-04-27 14:49:12 +0300

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	kotlin("jvm") version "1.5.31"
+	kotlin("jvm") version "1.6.21"
 	id("ru.capjack.publisher") version "1.0.0"
 }
 
@@ -17,13 +17,13 @@ kotlin {
 
 dependencies {
 	implementation(kotlin("compiler-embeddable"))
-	implementation("ru.capjack.tool:tool-logging:1.5.0")
-	implementation("ru.capjack.tool:tool-lang:1.11.1")
-	implementation("ru.capjack.tool:tool-utils:1.7.0")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
-	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.0")
+	implementation("ru.capjack.tool:tool-lang:1.12.0")
+	implementation("ru.capjack.tool:tool-logging:1.6.0")
+	implementation("ru.capjack.tool:tool-utils:1.8.0")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
+	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.2")
 	
 	testImplementation(kotlin("test"))
-	testImplementation("ch.qos.logback:logback-classic:1.2.7")
-	testImplementation("ru.capjack.tool:tool-biser:1.1.0")
+	testImplementation("ch.qos.logback:logback-classic:1.2.11")
+	testImplementation("ru.capjack.tool:tool-biser:1.5.0")
 }
