4caa3c5c1fc1d83781073c13c612024a625d9a56
Alexey Chuprin <a.chuprin@capjack.ru>
2022-08-15 17:38:02 +0300

    jvm 17 kotlin 1.7.10 gradle 7.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	kotlin("jvm") version "1.6.21"
+	kotlin("jvm") version "1.7.10"
 	id("ru.capjack.publisher") version "1.0.0"
 }
 
@@ -12,18 +12,18 @@ repositories {
 }
 
 kotlin {
-	target.compilations.all { kotlinOptions.jvmTarget = "11" }
+	target.compilations.all { kotlinOptions.jvmTarget = "17" }
 }
 
 dependencies {
 	implementation(kotlin("compiler-embeddable"))
-	implementation("ru.capjack.tool:tool-lang:1.12.0")
-	implementation("ru.capjack.tool:tool-logging:1.6.0")
-	implementation("ru.capjack.tool:tool-utils:1.8.0")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
-	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.2")
+	implementation("ru.capjack.tool:tool-lang:1.13.1")
+	implementation("ru.capjack.tool:tool-logging:1.7.0")
+	implementation("ru.capjack.tool:tool-utils:1.9.0")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
+	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.3")
 	
 	testImplementation(kotlin("test"))
 	testImplementation("ch.qos.logback:logback-classic:1.2.11")
-	testImplementation("ru.capjack.tool:tool-biser:1.5.0")
+	testImplementation("ru.capjack.tool:tool-biser:1.6.0")
 }
